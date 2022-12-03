82a96ce7b08b105b0438c1da824edd1d9840331c
Jessica Vaughn <jessica.vaughn619@gmail.com>
2022-12-02 19:01:56 -0700

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(drivers, names) {
+    return drivers.filter((name) => 
+    name.toLowerCase().includes(names.toLowerCase()));
+};
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter((letter) =>
+        letter === letters)
+    };
+    
+function matchName(drivers, driverNames) {
+    return drivers.filter((driverName) =>
+    drivers.name === driverNames);        
+};
